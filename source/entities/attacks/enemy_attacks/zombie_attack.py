4f5c1f6d6c004b3ebbec1319da1cdbfe55e704ed
Maurits <mauritsseelen@hotmail.com>
2023-04-02 11:19:23 +0200

    less dmg

--- source/entities/attacks/enemy_attacks/zombie_attack.py
+++ source/entities/attacks/enemy_attacks/zombie_attack.py
@@ -5,5 +5,5 @@ class ZombieAttack(RangedAttack):
     def __init__(self):
         super().__init__("space_shooter", "meteorGrey_med1")
 
-        self.damage = 25
+        self.damage = 7
         self.sound = arcade.load_sound(":resources:sounds/hurt5.wav")
