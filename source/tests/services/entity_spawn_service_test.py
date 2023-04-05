ec26110a04d29689d44e2bb72de6d20f3921e01e
Maurits <mauritsseelen@hotmail.com>
2023-04-05 21:43:51 +0200

    tests

--- source/tests/services/entity_spawn_service_test.py
+++ source/tests/services/entity_spawn_service_test.py
@@ -11,5 +11,21 @@ class EntitySpawnServiceTest(unittest.TestCase):
 
         assert type(z) is ZombieEnemy
 
+    def test_spawning_multiple_enemies(self):
+        x = 5
+        for _ in range(x):
+            self.s.spawn_zombie_enemy()
+
+        assert len(self.s.enemy_spawn_locations) == x
+
+    def test_clear_spawn_locations(self):
+        x = 3
+        for _ in range(x):
+            self.s.spawn_zombie_enemy()
+
+        self.s.clear_spawn_locations()
+
+        assert len(self.s.enemy_spawn_locations) == 0
+
     def setUp(self) -> None:
         self.s = EntitySpawnService()
