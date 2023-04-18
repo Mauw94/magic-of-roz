e6ead077a90ce090f13efdb09b8fc29f777f2f02
Maurits Seelen <mauritsseelen@hotmail.com>
2023-04-18 12:38:23 +0200

    mock mongodb + charactermanagertests

--- source/tests/managers/data_managers/characers_manager_tests.py
+++ source/tests/managers/data_managers/characers_manager_tests.py
@@ -1,12 +1,22 @@
 import unittest
-
+import mongomock
 from managers.data_managers.characters_manager import CharactersManager
 
 
 class CharactersManagerTests(unittest.TestCase):
     
     def test_getting_player_characters(self):
-        pass
+        characters = self.manager.get_player_characters()
+        
+        assert len(characters) == 2
 
     def setUp(self):
-        self.manager = CharactersManager()
+        self.manager = CharactersManager(self.__mock_collection())
+    
+    def __mock_collection(self):
+        collection = mongomock.MongoClient().db.collection
+        objects = [dict({}), dict({})]
+        for o in objects:
+            collection.insert_one(o)
+        
+        return collection
