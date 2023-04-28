8f0bac661d93b41f5360aa4397dea5e3fc3797db
Maurits Seelen <mauritsseelen@hotmail.com>
2023-04-28 18:16:43 +0200

    create mock character

--- source/tests/services/apply_item_effect_service_tests.py
+++ source/tests/services/apply_item_effect_service_tests.py
@@ -1,22 +1,15 @@
 import unittest
-import mongomock
 
 from services.apply_item_effect_service import ApplyItemEffectService
-from entities.classes.warrior import Warrior
 from entities.items.consumables.health_globe import HealthGlobe
-from managers.data_managers.characters_manager import CharactersManager
-from entities.classes.class_type import ClassTypeEnum
 from entities.items.consumables.mana_globe import ManaGlobe
+from tests.helpers.char_creator import create_character
 
 
 class ApplyItemEffectServiceTests(unittest.TestCase):
 
     def test_apply_item_effect_health_globe(self):
-        self.chars_manager.save_player_character_info(
-            "test1", ClassTypeEnum.WARRIOR)
-        c = self.chars_manager.get_player_characters()
-        p = self.chars_manager.load_player_object(c[0])
-        p.setup()
+        p = create_character()
 
         assert p is not None
         assert p.resource_manager.get_cur_hp() == 110
@@ -30,10 +23,7 @@ class ApplyItemEffectServiceTests(unittest.TestCase):
         assert p.resource_manager.get_cur_hp() == 110
 
     def test_apply_item_effect_mana_globe(self):
-        self.chars_manager.save_player_character_info("test1", ClassTypeEnum.WARRIOR)
-        c = self.chars_manager.get_player_characters()
-        p = self.chars_manager.load_player_object(c[0])
-        p.setup()
+        p = create_character()
         
         assert p is not None
         assert p.resource_manager.get_cur_mana() == 60
@@ -48,7 +38,4 @@ class ApplyItemEffectServiceTests(unittest.TestCase):
     
     def setUp(self):
         self.service = ApplyItemEffectService()
-        self.chars_manager = CharactersManager(self.__mock_collection())
 
-    def __mock_collection(self):
-        return mongomock.MongoClient().db.collection
